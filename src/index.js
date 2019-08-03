501d8c61ac989456d6a479a3cca324dcd13baad2
anthony pegues <apegraduate8@gmail.com>
2019-08-03 13:47:37 -0400

    first react 2019

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import Approval from "./Approval";
+import Faker from "faker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+      <div className=" ui container comments">
+        <Approval>
+          <CommentDetail
+          author="Sam"
+          time="Today at 4:00AM"
+          content="what"
+          image={Faker.image.avatar()}
+          />
+        </Approval>
+        <CommentDetail
+          author="Tom"
+          time="Today at 5:00PM"
+          content="when"
+          image={Faker.image.avatar()}
+          />
+        <CommentDetail
+          author="Ant"
+          time="Today at 2:00PM"
+          content="why"
+          image={Faker.image.avatar()}
+          />
+      </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+// https://semantic-ui.com/
+// cdn - https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css
+// https://github.com/marak/Faker.js/
+// -> stage 2 proposal class properties
+// intersection observers - image loading
+
+
